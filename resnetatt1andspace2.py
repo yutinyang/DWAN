c952181e0f2d6456cc83a7160c8d0c183f3cb805
yutinyang <37372406+yutinyang@users.noreply.github.com>
2022-12-10 08:07:00 +0000

    example_models

---
+++ resnetatt1andspace2.py
@@ -0,0 +1,344 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dwtmodel.waveletpro import Downsamplewave,Downsamplewave1
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        # self.att = Waveletatt(in_planes=planes)
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        # out = self.att(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+class Waveletatt(nn.Module):
+    r""" Patch Merging Layer.
+
+    Args:
+        input_resolution (tuple[int]): Resolution of input feature.
+        dim (int): Number of input channels.
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+    """
+
+    def __init__(self, input_resolution=224, in_planes=3, norm_layer=nn.LayerNorm):
+        super().__init__()
+        wavename = 'haar'
+        self.input_resolution = input_resolution
+
+        # self.dim = dim
+        # self.reduction = nn.Linear(4 * dim, 2 * dim, bias=False)
+        # self.norm = norm_layer(4 * dim)
+        # self.low_dim = nn.Conv2d(4 * in_planes, in_planes,kernel_size=3, stride=1,padding=1)
+        self.downsamplewavelet = nn.Sequential(*[nn.Upsample(scale_factor=2),Downsamplewave1(wavename=wavename)])
+        # self.downsamplewavelet = Downsamplewave(wavename=wavename)
+        # self.conv1 = nn.Conv2d()
+        # self.ac = nn.Sigmoid()
+        # self.bn = nn.BatchNorm2d(in_planes)
+        self.fc = nn.Sequential(
+            nn.Linear(in_planes, in_planes // 2, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_planes // 2, in_planes, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        """
+        x: B, H*W, C
+        """
+        xori = x
+        B, C, H, W= x.shape
+        # H, W = self.input_resolution
+        # B, L, C = x.shape
+        # assert L == H * W, "input feature has wrong size"
+        # assert H % 2 == 0 and W % 2 == 0, f"x size ({H}*{W}) are not even."
+        x = x.view(B, H, W, C)
+        x = x.permute(0, 3, 2, 1)
+        # x0,x1,x2,x3 = Downsamplewave(x)
+        ##x0,x1,x2,x3= self.downsamplewavelet(x)
+        y = self.downsamplewavelet(x)
+        y = self.fc(y).view(B, C, 1, 1)  # torch.Size([64, 256])-->torch.Size([64, 256, 1, 1])
+        y = xori * y.expand_as(xori)
+        # x0 =x0.permute(0,2,3,1)
+        # y = x0
+        # y = torch.sum(y, dim=[2, 3])
+        # y = self.fc(y).view(B, y.shape[1], 1, 1)
+        # return y
+
+        # y = torch.cat([x0, x1,x2,x3], 1)#torch.Size([64, 1024, 28, 28])
+        # # y = self.low_dim(y)
+        # y = torch.sum(y, dim=[2, 3])
+        # y = self.fc(y).view( B, y.shape[1], 1, 1)
+        # return y
+        ##y = torch.add (torch.add (torch.add (x1, x2), x3), x0)#torch.Size([64, 1024, 28, 28])
+        # y = self.fc(y).view( B, C, 1, 1)
+        # y = torch.mul(x, y)
+        # y = self.bn(y)
+        # y = self.ac(y)
+        # y = self.fc(y)
+        # return x*y
+        return y
+
+class Waveletattspace(nn.Module):
+    r""" Patch Merging Layer.
+
+    Args:
+        input_resolution (tuple[int]): Resolution of input feature.
+        dim (int): Number of input channels.
+        norm_layer (nn.Module, optional): Normalization layer.  Default: nn.LayerNorm
+    """
+
+    def __init__(self, input_resolution=224, in_planes=3, norm_layer=nn.LayerNorm):
+        super().__init__()
+        wavename = 'haar'
+        self.input_resolution = input_resolution
+
+        # self.dim = dim
+        # self.reduction = nn.Linear(4 * dim, 2 * dim, bias=False)
+        # self.norm = norm_layer(4 * dim)
+        # self.low_dim = nn.Conv2d(4 * in_planes, in_planes,kernel_size=3, stride=1,padding=1)
+        self.downsamplewavelet = nn.Sequential(*[nn.Upsample(scale_factor=2),Downsamplewave(wavename=wavename)])
+        # self.downsamplewavelet = Downsamplewave(wavename=wavename)
+        # self.conv1 = nn.Conv2d()
+        # self.ac = nn.Sigmoid()
+        # self.bn = nn.BatchNorm2d(in_planes)
+        self.fc = nn.Sequential(
+            # nn.Linear(in_planes, in_planes // 2, bias=False),
+            # nn.ReLU(inplace=True),
+            # nn.Linear(in_planes // 2, in_planes, bias=False),
+            nn.Conv2d(in_planes*2, in_planes//2, kernel_size=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_planes//2, in_planes,kernel_size=1,padding= 0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        """
+        x: B, H*W, C
+        """
+        xori = x
+        B, C, H, W= x.shape
+        # H, W = self.input_resolution
+        # B, L, C = x.shape
+        # assert L == H * W, "input feature has wrong size"
+        # assert H % 2 == 0 and W % 2 == 0, f"x size ({H}*{W}) are not even."
+        x = x.view(B, H, W, C)
+        x = x.permute(0, 3, 2, 1)
+        # x0,x1,x2,x3 = Downsamplewave(x)
+        ##x0,x1,x2,x3= self.downsamplewavelet(x)
+        y = self.downsamplewavelet(x)
+
+        y = self.fc(y) # torch.Size([64, 256])-->torch.Size([64, 256, 1, 1])
+        # y = self.fc(y).view(B, C, 1, 1)  # torch.Size([64, 256])-->torch.Size([64, 256, 1, 1])
+        y = xori * y.expand_as(xori)
+        # x0 =x0.permute(0,2,3,1)
+        # y = x0
+        # y = torch.sum(y, dim=[2, 3])
+        # y = self.fc(y).view(B, y.shape[1], 1, 1)
+        # return y
+
+        # y = torch.cat([x0, x1,x2,x3], 1)#torch.Size([64, 1024, 28, 28])
+        # # y = self.low_dim(y)
+        # y = torch.sum(y, dim=[2, 3])
+        # y = self.fc(y).view( B, y.shape[1], 1, 1)
+        # return y
+        ##y = torch.add (torch.add (torch.add (x1, x2), x3), x0)#torch.Size([64, 1024, 28, 28])
+        # y = self.fc(y).view( B, C, 1, 1)
+        # y = torch.mul(x, y)
+        # y = self.bn(y)
+        # y = self.ac(y)
+        # y = self.fc(y)
+        # return x*y
+        return y
+
+
+class ResNetCIFAR(nn.Module):
+    """This is a variation of ResNet for CIFAR database.
+    Indeed, the network defined in Sec 4.2 performs poorly on CIFAR-100. 
+    This network is similar to table 1 without the stride and max pooling
+    to avoid to reduce too much the input size.
+    
+    This modification have been inspired by DenseNet implementation 
+    for CIFAR databases.
+    """
+    def __init__(self, layers, num_classes=1000, levels=4):
+        block = BasicBlock
+        self.inplanes = 64
+        super(ResNetCIFAR, self).__init__()
+
+        self.levels = levels
+        if(self.levels != 4 and self.levels != 3):
+            raise "Impossible to use this number of levels"
+
+        # Same as Densetnet
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.att1 = Waveletatt(in_planes=64)
+        self.attspace1 = Waveletattspace(in_planes=64)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=1)
+        self.att2 = Waveletatt(in_planes=128)
+        self.attspace2 = Waveletattspace(in_planes=128)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        if self.levels == 4:
+            self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+            self.avgpool = nn.AvgPool2d(4, stride=1)
+            self.fc = nn.Linear(12800, num_classes)
+        else:
+            # 3 levels
+            self.avgpool = nn.AvgPool2d(8, stride=1)
+            self.fc = nn.Linear(256 * block.expansion, num_classes)
+
+        self.att3 = Waveletatt(in_planes=256)
+        self.att4 = Waveletatt(in_planes=512)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        # x = self.bn1(x)
+        # x = self.relu(x)
+        # x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.att2(x)
+        x = self.attspace2(x)
+        # x = self.att2(x)
+        x = self.layer3(x)
+        # x = self.att3(x)
+        if self.levels == 4:
+            x = self.layer4(x)
+            # x = self.att4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+class ResNetCIFARNormal(nn.Module):
+
+    def __init__(self, layers, num_classes=1000):
+        block = BasicBlock
+        self.inplanes = 16
+        super(ResNetCIFARNormal, self).__init__()
+        # raise "It is not possible to use this network"
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+        #                       bias=False)
+        #self.bn1 = nn.BatchNorm2d(64)
+        #self.relu = nn.ReLU(inplace=True)
+        #self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Same as Densetnet
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+
+        self.layer1 = self._make_layer(block, 16, layers[0], stride=2)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.avgpool = nn.AvgPool2d(4, stride=1)
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        #x = self.bn1(x)
+        #x = self.relu(x)
+        #x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        #x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+#
+# def dwt_ll(self,x):
+#     new_x = []
+#     for i in range (x.date.shape[0]):
+#         y = x[i, :, :, :]
+#         ll = dwtll(y)
+#         new_x.append(ll)
+#     return new_x
+
+
+
